0ab12d28eedad185ce3107d70efd6c9735d561d2
Khoirul Hamdani <khoirulh1610@gmail.com>
2021-02-09 06:03:55 +0800

    up

--- app/Http/Controllers/TripayController.php
+++ app/Http/Controllers/TripayController.php
@@ -10,28 +10,130 @@ use App\Models\Chat;
 class TripayController extends Controller
 {
 
-    public function __construct(Request $request)
+    public function __construct()
     {
-        // $apikey  = $_SERVER['apiKey'] ?? '';
-        // if($apikey != 'YXV0b3Jla2Jlci5jb20='){
-        //     return array('status' => 'Not Autorized');
-        //     exit;
-        // }
-        return $request;
-        
+         
     }
 
-
-    public function GetInstruction(Request $request)
+    public function index(Request $request)
     {
-        $req = str_replace(\Request::url(), '', \Request::fullUrl()) ?? '';
-        $curl = curl_init();
-        $transaction = Transaction::where('id',$request->id)->first();
+    	if(!$this->CekAPI($request)){
+    		return response()->json(['message'=>'Unauthorized '.$request->ip()]);
+    	}    	
+    	if(!$request->group_id){
+    		return response()->json(['message'=>'Parameter is Missing']);
+    	}
+
+    	$transaction = Transaction::where('group_id',$request->group_id)->first();
+		if(!$transaction){
+    		return response()->json(['message'=>'Transaction Not Found']);
+    	}
+
         if($transaction->bot=='Select Payment'){
             $payment = Payment::where('id',$transaction->bot_cmd)->first();
         }
         if(!$payment){
-            exit;
+            return response()->json(['message'=>'Payment Not Found']);
+        }    	
+        
+    	return $this->chanel($request);
+    }
+
+    function CekAPI($request){
+    	if($request->header('apikey')=='6116afedcb0bc31083935c1c262ff4c9' || 
+    		in_array($request->ip(), array('147.139.192.236','36.92.209.137'))){
+    		return true;
+    	}else{    		
+    		return false;
+    	}
+    }
+    
+
+    function getCurl($url,$request){
+    	$curl = curl_init();
+    	 curl_setopt_array($curl, array(
+        CURLOPT_FRESH_CONNECT     => true,
+        CURLOPT_URL               => $url,
+        CURLOPT_RETURNTRANSFER    => true,
+        CURLOPT_HEADER            => false,
+        CURLOPT_HTTPHEADER        => array(
+            "Authorization: Bearer ".ENV('tripay_apikey','')
+        ),
+        CURLOPT_FAILONERROR       => false
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
+        return $response;
+    }
+
+    function chanel($request){
+    	$req = str_replace(\Request::url(), '', \Request::fullUrl());
+    	$url = "https://payment.tripay.co.id/".ENV('tripay_mode','api')."/merchant/payment-channel".$req;
+    	$hasil = 'a';
+        $resp = $this->getCurl($url,$request);          
+        $resp = json_decode($resp);
+        $i = 1;
+        if($resp->success==true){
+            foreach ($resp->data as $row) {                
+                $Payment = Payment::where('code',$row->code)->first();
+                if(!$Payment){
+                    $Payment = new Payment();
+                }
+                $Payment->group = $row->group;
+                $Payment->code = $row->code;
+                $Payment->name = $row->name;
+                $Payment->type = $row->type;
+                $Payment->type = $row->type;
+                $Payment->charged_to = $row->charged_to;
+                $Payment->fee_flat = $row->fee->flat;
+                $Payment->fee_percent = $row->fee->percent;
+                $Payment->save();
+            }
+        }        
+        $Payment = Payment::get();        
+        $transaction = Transaction::where('group_id',$request->group_id)->whereNotNull('buyer_whatsapp')->first();                
+        if($transaction){
+            $hasil .= "@".preg_replace('/^0/i', '62', $transaction->buyer_whatsapp)."\r\n".
+                        "Berikut adalah rincian pembayaran kakak : \r\n\r\n".
+                        "Harga Produk : Rp. ".number_format($transaction->harga_produk)."\r\n".
+                        "Biaya Rekber : Rp. ".number_format($transaction->fee)."\r\n".
+                        "Total : Rp. ".number_format($transaction->harga_produk + $transaction->fee)."\r\n\r\n".
+                        "Metode Pembayaran : \r\n";
+            foreach ($Payment as $row) {
+                $hasil .= "*".$row->id.'. '.trim(str_replace(['Virtual Account','(Open Payment)'],'',$row->name))."*\r\n"??'';
+            }
+            $hasil .= "\r\nSilahkan pilih metode pembayaran yang kakak mau dengan mengetik nomornya.\r\n\r\n".
+                        "Contoh : \r\nKetik *1* untuk memilih pembayaran melalui bank *Maybank*";
+            $Chat = new Chat();
+	        $Chat->chatid = $request->group_id;
+	        $Chat->message = $hasil;
+	        $Chat->type = 'out';
+	        $Chat->status = 0;
+	        $Chat->save();	        
+	        $data = array('status' => true, 'group_id'=> $request->group_id,'response'=>$hasil);
+	        return $data;	        
+        }else{        	
+        	$data = array('status' => false, 'group_id'=> $request->group_id,'response'=>'');
+        	return $data;	        
+        }
+
+    }
+
+
+    public function GetInstruction($request)
+    {
+    	
+        $req = str_replace(\Request::url(), '', \Request::fullUrl()) ?? '';
+        $curl = curl_init();
+        $transaction = Transaction::where('group_id',$request->group_id)->first();
+        if($transaction->bot=='Select Payment'){
+            $payment = Payment::where('id',$transaction->bot_cmd)->first();
+        }
+        if(!$payment){
+            return 'Payment not Found';
+            exit; 
         }
         curl_setopt_array($curl, array(
         CURLOPT_FRESH_CONNECT     => true,
@@ -50,24 +152,34 @@ class TripayController extends Controller
         curl_close($curl);
 
         // echo !empty($err) ? $err : $response;
-        $hasil = '<br>';
+        $hasil = '';
         $resp = json_decode($response);
         if(empty($err) && $resp->success==1){
             foreach ($resp->data as $row) {
-                $hasil .= "*".$row->title."*\r\n<br><br>"??'';
+                $hasil .= "*".$row->title."*\r\n"??'';
                 for ($i=0; $i < count($row->steps)-1; $i++) { 
-                    $hasil .="* ".$row->steps[$i]."\r\n<br>"??'';;
+                    $hasil .="* ".$row->steps[$i]."\r\n"??'';;
                 }
-                $hasil .= "<br>";
+                $hasil .= "\r\n";
             }
         }
         // return $hasil??'';
+        $chat = new Chat();
+        $chat->chatid = $request->group_id;
+        $chat->message = $hasil;
+        $chat->type = 'out';
+        $chat->status = 0;
+        $chat->save();
         return array('status' => true, 'group_id'=> $transaction->group_id,'response'=>$hasil);
         
     }
 
     public function GetChannel(Request $request)
     {
+    	if(!$this->CekAPI($request)){
+    		return response()->json(['message'=>'Unauthorized']);
+    	}
+
         $req = str_replace(\Request::url(), '', \Request::fullUrl());
         $curl = curl_init();
         curl_setopt_array($curl, array(
@@ -137,6 +249,10 @@ class TripayController extends Controller
 
     public function ReqTrx(Request $request)
     {
+    	if(!$this->CekAPI($request)){
+    		return response()->json(['message'=>'Unauthorized']);
+    	}
+
         $apiKey = ENV('tripay_apikey','');
         $privateKey = ENV('tripay_apikey','');
         $merchantCode = ENV('tripay_apikey','');
